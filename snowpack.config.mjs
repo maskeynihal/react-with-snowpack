ea0f0485ba37e7e89484d94f5c4e5cd85b9d6eab
maskeynihal <maskeynihal@gmail.com>
2021-06-12 22:29:06 +0545

    Add optimize build with webpack plugin

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,8 +1,23 @@
 /**@type {import('snowpack').SnowpackUserConfig} */
 export default {
   mount: {
-    public: "/",
-    src: "/dist",
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
-  plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-dotenv"],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-webpack',
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {},
+  devOptions: {},
+  buildOptions: {},
 };
